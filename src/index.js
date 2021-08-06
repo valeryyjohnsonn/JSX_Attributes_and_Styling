5fc1fc6b85c972fd9ca5a5d030338705bdd63d9d
valeryyjohnsonn <49496668+valeryyjohnsonn@users.noreply.github.com>
2021-08-06 16:22:47 -0500

    Practicing Styling Elements

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img =
+  "https://i.picsum.photos/id/1000/5626/3635.jpg?hmac=qWh065Fr_M8Oa3sNsdDL8ngWXv2Jb-EE49ZIn6c0P-g";
+
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <h1 className="heading" contentEditable="false" spellCheck="false">
+      Some Random Images
+    </h1>
+    <div>
+      <img alt="image1" src={img}></img>
+      <img alt="image2" src="https://picsum.photos/seed/picsum/200/300"></img>
+      <img alt="image3" src="https://picsum.photos/200/300?grayscale"></img>
+    </div>
   </div>,
   document.getElementById("root")
 );
